3cc5493c65c6830e880cbe8e29c8946989ee1582
misganaoj@gmail.com <misganaoj@gmail.com>
2026-05-22 15:59:36 +0100

    standardize plot

--- VITA.L_dataset_script.R
+++ VITA.L_dataset_script.R
@@ -232,7 +232,6 @@ ard.by.bmi2 <- avg_comparisons(
   mutate(subgroup = "Non-obese(BMI < 30)") %>%
   select(subgroup, estimate, conf.low, conf.high, p.value)
 
-
 #***Forest plot: Risk modelling***#
 vital.plot.HTE <- bind_rows(overall.ard,
                             ard.by.risk,
